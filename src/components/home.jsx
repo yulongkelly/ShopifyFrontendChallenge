ce8b7ffd9c9db3eb46c2006406718465770066e7
Kelly Wu <kelly.wu@edu.uwaterloo.ca>
2021-01-16 10:51:10 -0500

    set the nominations from home to be a new variable in movie

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,14 +1,19 @@
-import React, { useState, useDebugValue } from "react";
-import { Button } from "reactstrap";
+import React, { useState, useEffect } from "react";
 import { IconButton, InputAdornment, TextField } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import axios from "axios";
-import Movie from "./movie";
+import "./home.css";
+import { Movie } from "./movie";
+import io from "socket.io-client";
 
 function Home() {
+  console.log("begin");
+  const [socket, setSocket] = useState(null);
   const [movie, setMovie] = useState("");
   const [currentMovie, setCurrentMovie] = useState("");
   const [movies, setMovies] = useState([]);
+  const [nominationList, setNominationList] = useState([]);
+  const [nominations, setNominations] = useState(new Set());
 
   function handleInput(event) {
     const target = event.target;
@@ -33,28 +38,65 @@ function Home() {
       .catch((err) => {});
   }
 
+  useEffect(() => {
+    setSocket(io("http://localhost:3000"));
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on("nominationsChanged", () => {
+      const tmp = [];
+      for (const nomination of nominations) {
+        tmp.push(
+          <li>
+            <Movie
+              movie={nomination}
+              nominations={nominations}
+              action="remove"
+            />
+          </li>
+        );
+      }
+      setNominationList(tmp);
+    });
+  }, [socket]);
+
   return (
-    <div className="">
+    <div className="omdb">
       <div className="search">
-        <TextField
-          label="Movie"
-          value={movie}
-          onChange={handleInput}
-          onKeyPress={handleKeyPress}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment>
-                <IconButton>
-                  <SearchIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
+        <div style={{ margin: 20 }}>
+          <TextField
+            fullWidth={true}
+            label="Movie"
+            value={movie}
+            onChange={handleInput}
+            onKeyPress={handleKeyPress}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <IconButton>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </div>
         {currentMovie ? <h5>Results for "{currentMovie}"</h5> : ""}
-        {movies.map((m) => {
-          return <Movie movie={m} />;
+        {movies.map((movie) => {
+          return (
+            <Movie
+              movie={movie}
+              nominations={nominations}
+              action="Nominate"
+              socket={socket}
+            />
+          );
         })}
+      </div>
+      <div className="nominations">
+        <h2>Nominations</h2>
+        <ul>{nominationList}</ul>
       </div>
     </div>
   );
--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Movie(props) {
+export function Movie(props) {
+  const [color, setColor] = useState("black");
+  const [nominations, setNominations] = useState(props.nominations);
+  console.log(nominations);
+
+  function handleClick() {
+    if (props.action === "remove") {
+      setNominations((prenominations) => prenominations.delete(props.movie));
+    } else if (props.action === "Nominate") {
+      if (nominations.size >= 5) {
+        // pop up a baner
+      } else if (!nominations.has(props.movie)) {
+        setColor("grey");
+        console.log("after");
+        setNominations((prenominations) => prenominations.add(props.movie));
+      }
+    }
+  }
+
+  useEffect(() => {
+    console.log("in---");
+    props.socket.emit("nominationsChanged");
+  }, [nominations]);
+
   return (
     <div>
-      {console.log(props.movie)}
       <p>
-        {props.movie.Title} ({props.movie.Year})<button>Nominate</button>
+        {props.movie.Title} ({props.movie.Year})
+        <button style={{ color: color, borderRadius: 5 }} onClick={handleClick}>
+          {props.action}
+        </button>
       </p>
     </div>
   );
 }
-
-export default Movie;
